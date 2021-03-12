13bc218ae7b0538aa4243a62b4a297e73e2ce176
rafaalvesf <rafael.alves.1@hotmail.com>
2021-03-12 18:41:52 -0300

    incluindo métodos e parametros das classes

--- src/classe/data.java
+++ src/classe/data.java
@@ -1,6 +1,22 @@
 package classe;
 
 public class data {
-    int dia, ano;
-    String mes;
+    int dia, mes, ano;
+
+    data() {
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+    }
+    data(int diaInicial, int mesInicial, int anoInicial){
+        dia = diaInicial;
+        mes = mesInicial;
+        ano = anoInicial;
+    }
+    String pegarDataInformada(){
+        return String.format("%d/%d/%d", dia, mes, ano);
+    }
+    void imprimirData(){
+        System.out.printf("%d/%d/%d", dia, mes, ano);
+    }
 }
--- src/classe/dataTeste.java
+++ src/classe/dataTeste.java
@@ -1,12 +1,14 @@
 package classe;
 
+import java.util.Scanner;
+
 public class dataTeste {
     public static void main(String[] args) {
-        data setData = new data();
-        setData.dia = 19;
-        setData.mes = "Janeiro";
-        setData.ano = 1996;
 
-        System.out.println("A data foi registrada " + setData.ano);
+        data setData = new data(19, 01, 1996);
+        data setData1 = new data();
+
+        System.out.println("A data informada foi: " + setData.pegarDataInformada());
+        System.out.println("A data informada foi: " + setData1.pegarDataInformada());
     }
 }
